603bc76343abb408a6dc70baabe5047879d84563
Agata Kubiak <aga.kubia@gmail.com>
2015-08-20 16:43:36 +0200

    Add mobile menu

--- source/javascripts/all.js
+++ source/javascripts/all.js
@@ -1,1 +1,21 @@
 //= require_tree .
+$( document ).ready(function() {
+  $('#menu-action, #close-nav-button, #mobile-menu').click(function(){
+    
+    $('#mobile-menu').fadeToggle(500);
+    $('nav').toggleClass('active-nav');
+    
+  });
+
+
+ $(window).scroll(function(){
+
+    var wScroll = $(this).scrollTop();
+    var offerSection = $('.offer-section').offset().top;
+
+    if(wScroll > offerSection - 200 ) {
+      $('.top-opacity-image').css('opacity', '1') ; }
+    if (wScroll > offerSection + 200 ) {
+      $('.bottom-opacity-image').css('opacity', '1'); }
+  });
+});
